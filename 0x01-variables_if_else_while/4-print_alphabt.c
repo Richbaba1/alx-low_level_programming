544ffbac80ac39e904ffc575fe4e99235ca1e0bc
Richbaba1 <adeniyir05@gmail.com>
2023-10-14 06:43:28 -0700

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,15 +7,14 @@
  */
 int main(void)
 {
-	char c;
+	int c;
 
-	c = 'a';
-
-	for (c = 'a'; c <= 'z'; c++)
+	for (c = 97; c < 123; c++)
 	{
-		if (c != 'q'; && c != 'e';)
+		if (c != 101 && c != 113)
+		{
 			putchar(c);
-			c++;
+		}
 	}
 	putchar('\n');
 	return (0);
